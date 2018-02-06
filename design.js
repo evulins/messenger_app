de88169474d5617ec6430934cb0fc786b985e043
Ewelina Sarad <evelinasarad@gmail.com>
2018-02-06 14:45:03 +0100

    chat js

--- design.js
+++ design.js
@@ -30,14 +30,14 @@ var conversations = {
       messages: [
         {
           text: 'Dawcik. Dlaczego śmierdzisz kupą?',
-          sentAt: 'Tue Jan 01 2018 08:31:38 GMT+0100 (CET)',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
           sentByMe: true,
           seen: false,
           delivered: false
         },
         {
           text: 'bo nie myłem tyłka od środy',
-          sentAt: 'Tue Jan 01 2018 08:31:57 GMT+0100 (CET)',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
           sentByMe: false,
           seen: false,
           delivered: false
@@ -45,7 +45,7 @@ var conversations = {
       ],
       lastMessage: {
         text: 'Hejka',
-        sentAt: 'Tue Jan 02 2018 21:39:57 GMT+0100 (CET)',
+        sentAt: 'Tue Feb 04 2018 13:02:21 GMT+0100 (CET)',
         sentByMe: false,
         seen: false,
         delivered: false
@@ -57,6 +57,22 @@ var conversations = {
         surname: '',
         avatar: 'stefka.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Tue Jan 30 2018 21:39:57 GMT+0100 (CET)',
@@ -70,6 +86,22 @@ var conversations = {
         surname: '',
         avatar: 'lola.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Tue Jan 30 2018 21:39:57 GMT+0100 (CET)',
@@ -83,6 +115,22 @@ var conversations = {
         surname: '',
         avatar: 'koty.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Tue Jan 30 2018 21:39:57 GMT+0100 (CET)',
@@ -96,6 +144,22 @@ var conversations = {
         surname: 'Bukiszon',
         avatar: 'buka.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Tue Jan 30 2018 21:39:57 GMT+0100 (CET)',
@@ -109,6 +173,22 @@ var conversations = {
         surname: '',
         avatar: 'jebulka.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Tue Jan 30 2018 21:39:57 GMT+0100 (CET)',
@@ -122,6 +202,22 @@ var conversations = {
         surname: '',
         avatar: 'ewu.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Mon Jan 29 2018 21:39:57 GMT+0100 (CET)',
@@ -135,6 +231,22 @@ var conversations = {
         surname: 'Dawcikowska',
         avatar: 'awatar.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Tue Jan 30 2018 21:39:57 GMT+0100 (CET)',
@@ -148,6 +260,22 @@ var conversations = {
         surname: 'Dawciskowka',
         avatar: 'awatar.jpg'
       },
+      messages: [
+        {
+          text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+          sentAt: 'Tue Feb 06 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: true,
+          seen: false,
+          delivered: false
+        },
+        {
+          text: 'bo nie myłem tyłka od środy',
+          sentAt: 'Tue Feb 05 2018 13:02:21 GMT+0100 (CET)',
+          sentByMe: false,
+          seen: false,
+          delivered: false
+        }
+      ],
       lastMessage: {
         text: 'Muszę napsiać bardzo długi teskt, żeby sprawdzić, czy się zawija lub skraca.',
         sentAt: 'Tue Jan 30 2018 21:39:57 GMT+0100 (CET)',
@@ -160,6 +288,7 @@ var conversations = {
    return conversations.list;
   },
   addMessage: function(friend, message) {
+    
     // jeśli konwersacja z tym znajomym jeszcze nie istnieje to
     // dodaj nową konwersację i
     // dodaj nową wiadomość do obiektu lastMessage
@@ -174,11 +303,13 @@ var conversations = {
     //     // przenieś wiadomość z obiektu lastMessage dla tej konwersacji do listy messages
     //     // zastąp wartości w obiekcie lastMessage dla tej konwersacji nową wiadomością
   },
-  getConversationWith(full_name) {
-    // np. full_name = "Dawid Dawcikowski"
-    // zaimplementuj funkcję, która zwróci obiekt konwersacji
-    // z osobą o imieniu i nazwisko takim jak w zmiennej full_name
+  getConversationWith: function(full_name) {
+    var result = conversations.list.filter(function (element) {
+      return element.friend.name + " " + element.friend.surname === full_name;
+    });
+    return result[0];
   }
+
 };
 
 // na podstawie danych z obiektu conversations
@@ -194,7 +325,7 @@ function showConversations() {
           <img src="avatars/${current.friend.avatar}">
         </div>
         <div class="textMessage">
-          <p class="name"><span>${current.friend.name}${current.friend.surname}</span></p>
+          <p class="name"><span>${current.friend.name} ${current.friend.surname}</span></p>
           <p class="text"><span>${current.lastMessage.text}</span></p>
         </div>
         <div class="messageDetails">
@@ -204,30 +335,98 @@ function showConversations() {
       </li>
     `;
     $("#messagesList").append(conversation);
-      $("li").click(function(event) {
-      event.preventDefault();
-      $(".messengerApp").hide();
-      $(".chat").show();
-      // 1. pobierz imię i nazwisko z HTML klikniętego <li> i zapisz to do zmiennej
-      // 2. wywołaj funkcję getConversationWith i przekaż jako parametr wcześniej pobrane imię i nazwisko
-      //    żeby pobrać obiekt konwersacji z tą osobą i zapisz go do zmiennej
-      // 3. wywołaj funkcję showChat i przekaż jako paramert obiekt conversation, który pobrałaś wcześniej
-    });
 
     $(".topBar #close").click(function(event){
        event.preventDefault();
       $(".chat").hide();
       $(".messengerApp").show();
     });
-  };
+  }
+
+  
 }
 
 function showChat(conversation) {
+  /*
+  {
+    friend: {
+      name: 'Dawid',
+      surname: 'Dawcikowski',
+      avatar: 'dawid.jpg'
+    },
+    messages: [
+      {
+        text: 'Dawcik. Dlaczego śmierdzisz kupą?',
+        sentAt: 'Tue Jan 01 2018 08:31:38 GMT+0100 (CET)',
+        sentByMe: true,
+        seen: false,
+        delivered: false
+      },
+      {
+        text: 'bo nie myłem tyłka od środy',
+        sentAt: 'Tue Jan 01 2018 08:31:57 GMT+0100 (CET)',
+        sentByMe: false,
+        seen: false,
+        delivered: false
+      }
+    ],
+    lastMessage: {
+      text: 'Hejka',
+      sentAt: 'Tue Jan 02 2018 21:39:57 GMT+0100 (CET)',
+      sentByMe: false,
+      seen: false,
+      delivered: false
+    }
+  }
+
+  */
   // dodaj kolejne wiadomości z listy conversation.messages jako dzieci elementu #chatMessages
   // wzoruj się na tym jak dodawane są konwersacje w funkcji showConversations
   // będziesz potrzebowała dwóch zmiennych:
   // * dla kody HTML który będzie wyświetlony dla twojej wiadomości
   // * dla kody HTML który będzie wyświetlony dla wiadomości znajomego
+  
+  var allMessages = conversation.messages.concat([conversation.lastMessage]);
+  allMessages.sort(function(a, b) {
+    var dateA = new Date(a.sentAt);
+    var dateB = new Date(b.sentAt);
+    return dateA - dateB;
+  });
+
+  for (var i = 0; i < allMessages.length; i++) {
+    var current = allMessages[i];
+    var wrappedDate = moment(new Date(current.sentAt));
+    var myMessage = `
+      <div class="me">
+          <ul>
+            <p class="date">${wrappedDate.fromNow()}</p>
+            <li class="myMessage"><span>${current.text}</span></li>
+          </ul>
+        </div>
+    `;
+
+    var reply = `
+      <div class="reply">
+        <p class="date">${wrappedDate.fromNow()}</p>
+        <div class="avatar">
+          <img src="avatars/${conversation.friend.avatar}">
+        </div>
+        <ul>
+          <li class="reMessage"><span>${current.text}</span></li>
+        </ul>
+      </div>
+    `;
+
+    $(".chatMessages").append(current.sentByMe ? myMessage : reply);
+  }
+}
+
+function clearChat() {
+  $(".chatMessages").empty();
+}
+
+function changeName(full_name) {
+  $(".chat .topBar a p.name").text(full_name);
 }
 
 function getIcon(dupa) {
@@ -260,4 +459,19 @@ $(".top .tools").click(function(event){
 (function() {
   console.log("DSsdds")
   showConversations();
+
+  $("li").click(function(event) {
+    event.preventDefault();
+    $(".messengerApp").hide();
+    $(".chat").show();
+    clearChat();
+    var person = $(this).find(".textMessage .name span").text();
+    changeName(person);
+    var conversation = conversations.getConversationWith(person);
+    showChat(conversation);
+  // 1. pobierz imię i nazwisko z HTML klikniętego <li> i zapisz to do zmiennej
+  // 2. wywołaj funkcję getConversationWith i przekaż jako parametr wcześniej pobrane imię i nazwisko
+  //    żeby pobrać obiekt konwersacji z tą osobą i zapisz go do zmiennej
+  // 3. wywołaj funkcję showChat i przekaż jako paramert obiekt conversation, który pobrałaś wcześniej
+  });
 })();
